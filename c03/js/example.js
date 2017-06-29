05b117c8aba9800f99e8910189c6a860db5cb897
Mr.Luuu <403635290@qq.com>
2017-06-29 19:43:32 +0800

    Update example.js

--- c03/js/example.js
+++ c03/js/example.js
@@ -24,25 +24,24 @@
 	var elEnds;
 	
 	function offerExpires(today) {
-		var weekFromToday, day, date, month, yeat, dayNames, monthNames;
+		var weekFromToday, day, date, month, year, dayNames, monthNames;
 		
 		weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
 		
-		dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Firday', 'Saturday'];
-		
+		dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 		monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 		
 		day = dayNames[weekFromToday.getDay()];
 		date = weekFromToday.getDate();
 		month = monthNames[weekFromToday.getMonth()];
-		yeat = weekFromToday.getFullYear();
+		year = weekFromToday.getFullYear();
 		
 		expiryMsg = 'Offer expires next ';
 		expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
 		return expiryMsg;
 	}
 	
-		today = new Date();                           
+		today = new Date();                  
 		elEnds = document.getElementById('offerEnds');
 		elEnds.innerHTML = offerExpires(today);  
 }())
